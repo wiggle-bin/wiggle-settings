cd8c37b93981a006bb8637d20a9ad619c57835a0
Vincent Kranendonk <git@studiorabota.com>
2024-02-11 13:26:02 +0100

    Fixed service file incorrect paths

--- src/wiggle_settings/install.py
+++ src/wiggle_settings/install.py
@@ -5,9 +5,9 @@ BOOTH_FILE = 'wiggle-settings-boot.sh'
 SERVICE_FILE = 'wiggle-settings.service'
 
 def install():
-    scriptFile = Path(__file__).parent / f"service/{BOOTH_FILE}.sh"
-    serviceFile = Path(__file__).parent / f"service/{SERVICE_FILE}"
-    os.system(f"sudo cp {scriptFile} /usr/bin/{BOOTH_FILE}.sh")
+    scriptFile = Path(__file__).parent / "service" / BOOTH_FILE
+    serviceFile = Path(__file__).parent / "service" / SERVICE_FILE
+    os.system(f"sudo cp {scriptFile} /usr/bin/{BOOTH_FILE}")
     os.system(f"sudo cp {serviceFile} /etc/systemd/user/{SERVICE_FILE}")
-    os.system("systemctl --user enable {SERVICE_FILE}")
-    os.system("systemctl --user start {SERVICE_FILE}")
+    os.system(f"systemctl --user enable {SERVICE_FILE}")
+    os.system(f"systemctl --user start {SERVICE_FILE}")
